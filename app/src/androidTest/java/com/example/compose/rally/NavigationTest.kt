ba7caea9b2b7dc2eeb08d85eaf00246e402e7bed
Drew Sommer <drew@sommerengineering.com>
2024-10-03 21:18:15 +0300

    test: Create test controller and navigator

--- app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/NavigationTest.kt
@@ -1,10 +1,35 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import junit.framework.TestCase.fail
 import org.junit.Rule
+import org.junit.Test
 
 class NavigationTest {
 
+    lateinit var testNavController: TestNavHostController
+
     @get:Rule
     val composeTestRule = createComposeRule()
+    
+    @Test
+    fun rallyNavHost() {
+        composeTestRule.setContent {
+
+            // create test navigation controller
+            testNavController = TestNavHostController(LocalContext.current)
+
+            // create test "navigator" to crawl composables
+            testNavController.navigatorProvider.addNavigator(
+                ComposeNavigator()
+            )
+
+            RallyNavHost(navController = testNavController)
+        }
+
+        fail()
+    }
 }
